c8d2363d5e9dee8d7a1fbf1571b1e1a7256363bb
Callum Bain <callum_bain@apple.com>
2025-07-29 15:23:15 +0100

    lightmode and darkmode toggle plus star background

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,14 @@
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* theme toggle */}
+      <ThemeToggle />
 
       {/* Background Effects */}
+      <StarBackground />
 
       {/* Navbar */}
 
