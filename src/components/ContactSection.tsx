5f80c0bce430485e232f77291c5023855346de54
Callum Bain <callum_bain@apple.com>
2025-07-30 21:30:17 +0100

    contact mobile screen handling

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -7,55 +7,61 @@ export const ContactSection = () => {
         <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">
           Contact <span className="text-primary">Information</span>
         </h2>
-        <div className="gap-12">
-          <div className="space-y-8">
-            <div className="space-y-6 justify-center">
-              <div className="flex justify-center space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <MailIcon className="h-6 w-6 text-primary" />
-                </div>
-                <div>
-                  <h4 className="font-medium">Email</h4>
-                  <a
-                    href="mailto:hello@gmail.com"
-                    className="text-muted-foreground hover:text-primary transition-colors"
-                  >
-                    hello@gmail.com
-                  </a>
-                </div>
-                <div className="p-3 rounded-full bg-primary/10">
-                  <PhoneCall className="h-6 w-6 text-primary" />
-                </div>
-                <div>
-                  <h4 className="font-medium">Phone</h4>
-                  <a
-                    href="tel:+447777777777"
-                    className="text-muted-foreground hover:text-primary transition-colors"
-                  >
-                    +44 7777777777
-                  </a>
-                </div>
-                <div className="p-3 rounded-full bg-primary/10">
-                  <MapPin className="h-6 w-6 text-primary" />
-                </div>
-                <div>
-                  <h4 className="font-medium">Location</h4>
-                  <a className="text-muted-foreground hover:text-primary transition-colors">
-                    London, UK
-                  </a>
-                </div>
-                <div className="p-3 rounded-full bg-primary/10">
-                  <Linkedin className="h-6 w-6 text-primary" />
-                </div>
-                <div>
-                  <h4 className="font-medium">LinkedIn</h4>
-                  <a
-                    href="www.mylinkedin.com"
-                    className="text-muted-foreground hover:text-primary transition-colors"
-                  >
-                    www.mylinkedin.com
-                  </a>
-                </div>
+        <div className="space-y-6 justify-center">
+          <div className="grid gap-6 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-items-center md:justify-items-start">
+            {/*  */}
+            <div className="flex items-center space-x-4">
+              <div className="p-3 rounded-full bg-primary/10">
+                <MailIcon className="h-6 w-6 text-primary" />
+              </div>
+              <div>
+                <h4 className="font-medium">Email</h4>
+                <a
+                  href="mailto:hello@gmail.com"
+                  className="text-muted-foreground hover:text-primary transition-colors"
+                >
+                  hello@gmail.com
+                </a>
+              </div>
+            </div>
+            {/*  */}
+            <div className="flex items-center space-x-4">
+              <div className="p-3 rounded-full bg-primary/10">
+                <PhoneCall className="h-6 w-6 text-primary" />
+              </div>
+              <div>
+                <h4 className="font-medium">Phone</h4>
+                <a
+                  href="tel:+447777777777"
+                  className="text-muted-foreground hover:text-primary transition-colors"
+                >
+                  +44 7777777777
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center space-x-4">
+              <div className="p-3 rounded-full bg-primary/10">
+                <MapPin className="h-6 w-6 text-primary" />
+              </div>
+              <div>
+                <h4 className="font-medium">Location</h4>
+                <a className="text-muted-foreground hover:text-primary transition-colors">
+                  London, UK
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center space-x-4">
+              <div className="p-3 rounded-full bg-primary/10">
+                <Linkedin className="h-6 w-6 text-primary" />
+              </div>
+              <div>
+                <h4 className="font-medium">LinkedIn</h4>
+                <a
+                  href="www.mylinkedin.com"
+                  className="text-muted-foreground hover:text-primary transition-colors"
+                >
+                  www.mylinkedin.com
+                </a>
               </div>
             </div>
           </div>
